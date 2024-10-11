d9300d2a7531da467b59a404028bb263ded9ef2d
Kevin Fox <kevinfox.texas@proton.me>
2024-10-11 12:51:46 -0500

    Setup Radix UI

--- app/historic-games/page.tsx
+++ app/historic-games/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Board from "../components/Board";
+import { Button } from "@radix-ui/themes";
 
 const HistoricGames = () => {
     return (
         <>
             <h1>Historic Games</h1>
-            <div>Table</div>
+            <div><Button>Add game</Button></div>
         </>
     );
 };
