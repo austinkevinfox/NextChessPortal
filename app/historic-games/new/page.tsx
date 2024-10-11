d714f793620d48e6f294835f970ec0cce7330c1b
Kevin Fox <kevinfox.texas@proton.me>
2024-10-11 16:28:08 -0500

    Handle Game form submission

--- app/historic-games/new/page.tsx
+++ app/historic-games/new/page.tsx
@@ -1,17 +1,51 @@
+"use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+interface NewHistoricGameForm {
+    title: string;
+    white: string;
+    black: string;
+    result: string;
+    moves: string;
+}
 
 const NewHistoricGame = () => {
+    const router = useRouter();
+    const { register, handleSubmit } = useForm<NewHistoricGameForm>();
+
+    const onSubmit = async (data: NewHistoricGameForm) => {
+        await axios.post("/api/games", data);
+        router.push("/historic-games");
+    };
+
     return (
-        <div className="max-w-lg px-4 space-y-3">
-            <TextField.Root placeholder="Title"></TextField.Root>
-            <TextField.Root placeholder="White player name"></TextField.Root>
-            <TextField.Root placeholder="Black player name"></TextField.Root>
-            <TextArea placeholder="Moves…" />
-            <Button>
-                Add new game
-            </Button>
-        </div>
+        <form
+            className="max-w-lg px-4 space-y-3"
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <TextField.Root
+                placeholder="Title"
+                {...register("title")}
+            ></TextField.Root>
+            <TextField.Root
+                placeholder="White player name"
+                {...register("white")}
+            ></TextField.Root>
+            <TextField.Root
+                placeholder="Black player name"
+                {...register("black")}
+            ></TextField.Root>
+            <TextField.Root
+                placeholder="Result"
+                {...register("result")}
+            ></TextField.Root>
+            
+            <TextArea placeholder="Moves…" {...register("moves")} />
+            <Button>Add new game</Button>
+        </form>
     );
 };
 
