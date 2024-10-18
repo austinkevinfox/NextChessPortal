6d4c282af7025207a1aac5f56a1c29ca02f083ad
Kevin Fox <kevinfox.texas@proton.me>
2024-10-18 17:21:06 -0500

    Style historic game details page

--- app/historic-games/[id]/page.tsx
+++ app/historic-games/[id]/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
+import GameStatusBadge from "../_components/GameStatusBadge";
 
 const GameDetailsPage = async ({ params }: { params: { id: string } }) => {
     const game = await prisma.game.findUnique({
@@ -11,11 +12,26 @@ const GameDetailsPage = async ({ params }: { params: { id: string } }) => {
 
     return (
         <>
-            <p>{game.title}</p>
-            <p>{game.white}</p>
-            <p>{game.black}</p>
-            <p>{game.result}</p>
-            <p>{game.addedAt.toString()}</p>
+            <Heading>{game.title}</Heading>
+            <Flex gap="2" my="2" align="center">
+                <GameStatusBadge status={game.result} />
+                <Text size="1">{game.eventDate.toDateString()}</Text>
+            </Flex>
+            {game.event && (
+                <Flex gap="2" my="2" align="center">
+                    <Text>Event:</Text>
+                    <Text>{game.event}</Text>
+                </Flex>
+            )}
+            <Flex gap="2" my="2" align="center">
+                <Text>White:</Text>
+                <Text>{game.white}</Text>
+            </Flex>
+            <Flex gap="2" my="2" align="center">
+                <Text>Black:</Text>
+                <Text>{game.black}</Text>
+            </Flex>
+            <Card>{game.moves}</Card>
         </>
     );
 };
