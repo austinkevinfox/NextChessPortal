243623a0a10ba8f24297d90ab9140af9becf22e9
Kevin Fox <kevinfox.texas@proton.me>
2024-10-19 07:49:01 -0500

    Add trophy icon to game status badge

--- app/historic-games/_components/GameStatusBadge.tsx
+++ app/historic-games/_components/GameStatusBadge.tsx
@@ -1,18 +1,26 @@
 import { Result } from "@prisma/client";
 import { Badge } from "@radix-ui/themes";
+import { HiOutlineTrophy } from "react-icons/hi2";
 
 const statusMap: Record<
     Result,
     { label: string; color: "gold" | "indigo" | "gray" | "violet" }
-    > = {
-        WHITE: {label: "White", color: "gold"},
-        BLACK: {label: "Black", color: "indigo"},
-        DRAW: {label: "Draw", color: "gray"},
-        IN_PROGRESS: {label: "In Progress", color: "violet"},
-    };
+> = {
+    WHITE: { label: "White", color: "gold" },
+    BLACK: { label: "Black", color: "indigo" },
+    DRAW: { label: "Draw", color: "gray" },
+    IN_PROGRESS: { label: "In Progress", color: "violet" },
+};
 
 const GameStatusBadge = ({ status }: { status: Result }) => {
-    return <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>;
+    return (
+        <Badge color={statusMap[status].color}>
+            {/^(WHITE|BLACK)$/.test(status) && (
+                <HiOutlineTrophy />
+            )}
+            {statusMap[status].label}
+        </Badge>
+    );
 };
 
 export default GameStatusBadge;
