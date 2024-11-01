a736e6c5562075a50c4018f214df5fa6bee46d7f
Kevin Fox <kevinfox.texas@proton.me>
2024-11-01 15:25:46 -0500

    Add status badge icons

--- app/historic-games/_components/GameStatusBadge.tsx
+++ app/historic-games/_components/GameStatusBadge.tsx
@@ -1,23 +1,32 @@
 import { Result } from "@prisma/client";
 import { Badge } from "@radix-ui/themes";
+import { ReactElement } from "react";
 import { HiOutlineTrophy } from "react-icons/hi2";
+import { IoTimerOutline } from "react-icons/io5";
+import { LuEqual } from "react-icons/lu";
 
 const statusMap: Record<
     Result,
-    { label: string; color: "gold" | "indigo" | "gray" | "violet" }
+    {
+        label: string;
+        color: "gold" | "indigo" | "gray" | "violet";
+        icon: ReactElement;
+    }
 > = {
-    WHITE: { label: "White", color: "gold" },
-    BLACK: { label: "Black", color: "indigo" },
-    DRAW: { label: "Draw", color: "gray" },
-    IN_PROGRESS: { label: "In Progress", color: "violet" },
+    WHITE: { label: "White", color: "gold", icon: <HiOutlineTrophy /> },
+    BLACK: { label: "Black", color: "indigo", icon: <HiOutlineTrophy /> },
+    DRAW: { label: "Draw", color: "gray", icon: <LuEqual /> },
+    IN_PROGRESS: {
+        label: "In Progress",
+        color: "violet",
+        icon: <IoTimerOutline />,
+    },
 };
 
 const GameStatusBadge = ({ status }: { status: Result }) => {
     return (
         <Badge color={statusMap[status].color}>
-            {/^(WHITE|BLACK)$/.test(status) && (
-                <HiOutlineTrophy />
-            )}
+            {statusMap[status].icon}
             {statusMap[status].label}
         </Badge>
     );
