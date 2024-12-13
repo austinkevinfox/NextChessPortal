e899619ff462725dbf736a106b71f64eadef2df5
Kevin Fox <kevinfox.texas@proton.me>
2024-12-13 17:38:07 -0600

    Add delete confirmation

--- app/historic-games/_components/grid-buttons/DeleteButton.tsx
+++ app/historic-games/_components/grid-buttons/DeleteButton.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { Box } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { FaTrashCan } from "react-icons/fa6";
+import { FaRegTrashCan } from "react-icons/fa6";
 import RoundHoverIcon from "./RoundHoverIcon";
-import GridButtonTooltip from "./tooltip/Tooltip";
 
 const DeleteButton = ({ gameId }: { gameId: number }) => {
     const router = useRouter();
 
-    const handleClick = async () => {
+    const deleteGame = async () => {
         await axios.delete(`/api/games/${gameId}`);
 
         router.push("/historic-games");
@@ -17,13 +16,34 @@ const DeleteButton = ({ gameId }: { gameId: number }) => {
     };
 
     return (
-        <GridButtonTooltip content="Delete game">
-            <Box onClick={handleClick}>
-                <RoundHoverIcon>
-                    <FaTrashCan />
-                </RoundHoverIcon>
-            </Box>
-        </GridButtonTooltip>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <button>
+                    <RoundHoverIcon>
+                        <FaRegTrashCan />
+                    </RoundHoverIcon>
+                </button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+                <AlertDialog.Description>
+                    Are you sure you want to delete this game? Delete action
+                    cannot be undone.
+                </AlertDialog.Description>
+                <Flex mt="4" gap="3">
+                    <AlertDialog.Cancel>
+                        <Button color="gray" variant="soft">
+                            Cancel
+                        </Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button color="red" onClick={deleteGame}>
+                            Delete Game
+                        </Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     );
 };
 
